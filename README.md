632970fe517510a19b3b659ac3a19778a29a032c
Tamas Mako <mako.tomi@gmail.hu>
2023-06-06 15:18:19 +0200

    selenium done

---
+++ README.md
@@ -0,0 +1,1 @@
+The program opens the website https://neuralnine.com and navigates to the book 'Python Bible 7 in 1'. Click on it and you will be taken to the Amazon website, where the program finds the price, which it writes into a txt file.
---
+++ app.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+optinons = Options()
+optinons.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=optinons)
+
+driver.get("https://neuralnine.com/")
+driver.maximize_window()
+
+links = driver.find_elements("xpath", "//a[@href]")
+for link in links:
+    if "Books" in link.get_attribute("innerHTML"):
+        link.click()
+        break
+    
+book_links = driver.find_elements("xpath", "//div[contains(@class, 'elementor-column-wrap')][.//h2[text()[contains(., '7 IN 1')]]][count(.//a)=2]//a")
+
+book_links[0].click()
+
+driver.switch_to.window(driver.window_handles[1])
+
+time.sleep(3)
+buttons = driver.find_elements("xpath", "//a[.//span[text()[contains(., 'Paperback')]]]//span[text()[contains(., '$')]]")
+
+
+with open("result.txt", "w") as file:
+    for button in buttons:
+        text = button.get_attribute("innerHTML").replace("&nbsp;", " ")
+        file.write(text + "\n")
